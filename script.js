4085aa797d8fc764106e2e24b60746dc85669fc6
Zinanshin Kaoher <54826833+kaohzina@users.noreply.github.com>
2021-10-16 15:32:19 -0500

    switch to bootstrap

--- script.js
+++ script.js
@@ -1,8 +1,2 @@
-fetch('api.openweathermap.org/data/2.5/onecall?lat=30.489772&lon=-99.771335'
-  )
-  .then(function(response){ 
-    return response.json();
-  })
-  .then(function(data) {
-    console.log(data);
-  });
+const apiKey = "e0ec3338bd31d20ecc0b95e95df665f8"
+  
