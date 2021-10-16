617420831f4a696a1eeabc0316ca83ab69ba524c
Zinanshin Kaoher <54826833+kaohzina@users.noreply.github.com>
2021-10-16 14:41:02 -0500

    add the api in the js

---
+++ script.js
@@ -0,0 +1,8 @@
+fetch('https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}'
+  )
+  .then(function(response){ 
+    return response.json();
+  })
+  .then(function(data) {
+    console.log(data);
+  });
